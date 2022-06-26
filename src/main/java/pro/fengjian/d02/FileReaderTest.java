9555fe7f1424a9d50ac51f019aa9644c5db84956
fengjian2705 <fengjian2705@sina.com>
2022-06-26 11:37:05 +0800

    feat: FileReader；

--- src/main/java/pro/fengjian/d02/FileReaderTest.java
+++ src/main/java/pro/fengjian/d02/FileReaderTest.java
@@ -15,10 +15,26 @@ public class FileReaderTest {
             /*int res = fr.read();
             System.out.println("读取到的单个字符是: " + (char) res);// 'a'*/
 
-            int res = 0;
+            /*int res = 0;
             while ((res = fr.read()) != -1) {
                 System.out.println("读取到的单个字符是: " + (char) res);
+            }*/
+
+            // 准备一个字符数组来保存读取到的数据内容
+            char[] cArr = new char[5];
+            // 期望读取字符数组的部分空间
+//            int res = fr.read(cArr, 1, 3);
+//            System.out.println("实际读取到的字符个数是: " + res);// 3
+//            for (char cv : cArr) {
+//                System.out.println("读取到的单个字符是: " + (char) cv);// 啥也没有 a e l 啥也没有
+//            }
+            // 期望读取整个字符数组
+            int res = fr.read(cArr);
+            System.out.println("实际读取到的字符个数是: " + res);// 5
+            for (char cv : cArr) {
+                System.out.println("读取到的单个字符是: " + (char) cv);// a e l l h
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
